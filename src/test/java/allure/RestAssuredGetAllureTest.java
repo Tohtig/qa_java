6b8bbed23e6df89377af0bcf7309ff80358c3a63
Anton Ippolitov <aippolitov@fil-it.ru>
2023-02-07 09:13:57 +0300

    Добавлен REST пример с allure нотациями. И mvn-surefire-plugin для отчетов.

---
+++ src/test/java/allure/RestAssuredGetAllureTest.java
@@ -0,0 +1,81 @@
+package allure;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Step;
+import io.qameta.allure.TmsLink;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestAssuredGetAllureTest {
+
+    String bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzlkOWFiYjgwODNjMzAwNDIzNDI0NmQiLCJpYXQiOjE2NzUzNjc4MjMsImV4cCI6MTY3NTk3MjYyM30.7KpzHPJGVGa2F6_vGfsazhjdZPnx30HT-JdE0xTjOuY";
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://qa-mesto.praktikum-services.ru";
+    }
+
+    @Test
+    @DisplayName("Check status code of /users/me")
+    @Description("Basic test for /users/me endpoint")
+    public void getMyInfoStatusCode() {
+        given()
+                .auth().oauth2(bearerToken)
+                .get("/api/users/me")
+                .then().statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Check user name")
+    @Description("Checking user name is very important")
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken)
+                .get("/api/users/me")
+                .then().assertThat().body("data.name",equalTo("Василий Васильев"));
+    }
+
+    @Test
+    @DisplayName("Check user name and print response body") // имя теста
+    @Description("This is a more complicated test with console output") // описание теста
+    @TmsLink("TestCase-112") // ссылка на тест-кейс
+    @Issue("BUG-985") // ссылка на баг-репорт
+    public void checkUserNameAndPrintResponseBody() {
+
+        Response response = sendGetRequestUsersMe();
+        // отправили запрос и сохранили ответ в переменную response - экземпляр класса Response
+
+        compareUserNameToText(response, "Василий Васильев");
+        // проверили, что в теле ответа ключу name соответствует нужное имя пользователя
+
+        printResponseBodyToConsole(response); // вывели тело ответа на экран
+
+    }
+
+    // метод для шага "Отправить запрос":
+    @Step("Send GET request to /api/users/me")
+    public Response sendGetRequestUsersMe(){
+        Response response =given().auth().oauth2(bearerToken).get("/api/users/me");
+        return response;
+    }
+
+    // метод для шага "Сравнить имя пользователя с заданным":
+    @Step("Compare user name to something")
+    public void compareUserNameToText(Response response, String username){
+        response.then().assertThat().body("data.name",equalTo(username));
+    }
+
+    // метод для шага "Вывести тело ответа в консоль":
+    @Step("Print response body to console")
+    public void printResponseBodyToConsole(Response response){
+        System.out.println(response.body().asString());
+    }
+
+} 
